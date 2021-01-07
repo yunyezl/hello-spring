c0f5411240a68e98a2b6760d13ff6826a1e61d75
yunyezl <20172062@sungshin.ac.kr>
2021-01-08 01:00:25 +0900

    스프링 데이터 JPA

--- src/test/java/com/example/hellospring/service/MemberServiceIntegrationTest.java
+++ src/test/java/com/example/hellospring/service/MemberServiceIntegrationTest.java
@@ -22,7 +22,6 @@ public class MemberServiceIntegrationTest {
     @Autowired MemberRepository memberRepository;
 
     @Test
-    @Commit
     void 회원가입() {
         //given
         Member member = new Member();
